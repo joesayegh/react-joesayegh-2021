1231e5c47984f1683d28be23e2025b744ae12f8f
Joseph Sayegh <joesayegh@gmail.com>
2022-06-14 12:34:04 +0100

    updates contact form

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,30 +1,47 @@
-import React from "react";
+import React from 'react';
 
-import "./Contact.scss";
+import './Contact.scss';
 
 const Contact = () => {
+	let testForm = document.querySelector('#contact form');
+
+	testForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const formData = new FormData(testForm);
+		fetch(testForm.getAttribute('action'), {
+			method: 'POST',
+			headers: {
+				Accept: 'application/x-www-form-urlencoded;charset=UTF-8',
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+			},
+			body: new URLSearchParams(formData).toString(),
+		}).then((res) => {
+			if (res) {
+				alert('Thanks for getting in touch. We’ll get back to you very soon.');
+			}
+		});
+	});
+
 	return (
 		<section className="contact-form bg-contact-form">
-			<div className="contact-form-wrapper">
+			<div id="contact" className="contact-form-wrapper">
 				<h2 className="text-center">Say Hello</h2>
-				<form name="contact" method="POST" data-netlify="true">
+				<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/#contact">
+					<input className="d-none" name="bot-field" />
 					<p>
-						<label htmlFor="name">
-							Name
-							<input id="name" type="text" name="name" />
-						</label>
-					</p>
-
-					<p>
-						<label htmlFor="email">
-							Email
-							<input id="email" type="email" name="email" />
+						<label>
+							Name: <input type="text" name="name" />
 						</label>
 					</p>
 					<p>
-						<label htmlFor="message">
-							Message
-							<textarea id="message" name="message" rows="4"></textarea>
+						<label>
+							Email: <input type="email" name="email" />
+						</label>
+					</p>
+					<p>
+						<label>
+							Message: <textarea name="message"></textarea>
 						</label>
 					</p>
 					<p>
